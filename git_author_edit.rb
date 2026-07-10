2c046df35f3cee168b1fe731187bec5f1c1bad26
blitterated <blitterated@protonmail.com>
2026-07-10 15:36:07 -0500

    Add a configuration class

--- git_author_edit.rb
+++ git_author_edit.rb
@@ -143,7 +143,12 @@ class AllCLIOptions
         description: opt[:description],
       )
 
-    puts "#{defs[opt[:symbol]].to_s}" if DBUG; puts if DBUG
+    puts "New option def:\n#{defs[opt[:symbol]].to_s}" if DBUG; puts if DBUG
+  end
+
+  if DBUG
+    puts "AllCLIOptions' option definitions"
+    p @option_defs
   end
 
   def self.get_def(opt_sym)
@@ -161,6 +166,16 @@ class AllCLIOptions
   def self.repo_url()  get_def :repo_url;  end
   def self.work_dir()  get_def :work_dir;  end
 
+  # Get definied option definitions as an array
+  def self.array
+    @option_defs.values.clone.freeze
+  end
+
+  # Hash of option symbols to option paramater keys
+  def self.symbols_and_param_keys
+    AllCLIOptions::array.map { |odef| [odef.symbol, odef.param_key] }.to_h
+  end
+
   # Iterate the option definitions
   def self.each
     @option_defs.each do |option|
@@ -170,11 +185,37 @@ class AllCLIOptions
 end
 
 
+class GitAuthorEditConfig
+  attr_reader :old_name, :old_email, :new_name, :new_email, :repo_url, :work_dir
+
+  def initialize(old_name:, old_email:, new_name:, new_email:, repo_url:, work_dir:)
+    @old_name  = old_name.freeze
+    @old_email = old_email.freeze
+    @new_name  = new_name.freeze
+    @new_email = new_email.freeze
+    @repo_url  = repo_url.freeze
+    @work_dir  = work_dir.freeze
+  end
+
+  def to_s
+    [
+      "GitAuthorEditConfig",
+      "  @old_name  = \"#{@old_name}\"",
+      "  @old_email = \"#{@old_email}\"",
+      "  @new_name  = \"#{@new_name}\"",
+      "  @new_email = \"#{@new_email}\"",
+      "  @repo_url  = \"#{@repo_url}\"",
+      "  @work_dir  = \"#{@work_dir}\"",
+    ].join("\n")
+  end
+end
+
+
 # Basically a wrapper class around OptionParser.
 class GitAuthorEditCLI
   def initialize
     # Default option values
-    @options = {AllCLIOptions::work_dir.param_key => DefaultWorkDir}
+    @option_vals = {AllCLIOptions::work_dir.param_key => DefaultWorkDir}
   end
 
   private def create_parser
@@ -187,18 +228,31 @@ class GitAuthorEditCLI
     end
   end
 
-  def parse(args)
-    option_parser = create_parser
+  private def map_option_vals_to_config
+    sym2pkey = AllCLIOptions::symbols_and_param_keys
 
+    GitAuthorEditConfig.new(
+      old_name:  @option_vals[sym2pkey[:old_name]],
+      old_email: @option_vals[sym2pkey[:old_email]],
+      new_name:  @option_vals[sym2pkey[:new_name]],
+      new_email: @option_vals[sym2pkey[:new_email]],
+      repo_url:  @option_vals[sym2pkey[:repo_url]],
+      work_dir:  @option_vals[sym2pkey[:work_dir]],
+    )
+  end
+
+  def parse(args)
+    config = GitAuthorEditCLI.new
+    option_parser = create_parser
     begin
-      option_parser.parse(args, into: @options)
+      option_parser.parse(args, into: @option_vals)
     rescue OptionParser::InvalidOption => opt_err
       puts "#{opt_err.message}\n".red
       puts option_parser.help
       exit 1
     end
 
-    @options
+    map_option_vals_to_config
   end
 
   def get_optparse_banner()
@@ -231,14 +285,15 @@ class GitAuthorEdit
     glob_opts = File::FNM_DOTMATCH
 
     Dir.chdir(@work_dir) do
-=begin
-# Shell command example of workflow.
 
-git clone git@ghblit:blitterated/tarmux.git
-cd tarmux
-git filter-repo --force --mailmap ../mailmap
-git push -u --force origin git@ghblit:blitterated/tarmux.git
-git remote add origin git@ghblit:blitterated/tarmux.git
+=begin
+    # Shell command example of workflow.
+
+    git clone git@ghblit:blitterated/tarmux.git
+    cd tarmux
+    git filter-repo --force --mailmap ../mailmap
+    git push -u --force origin git@ghblit:blitterated/tarmux.git
+    git remote add origin git@ghblit:blitterated/tarmux.git
 =end
     end
   end
@@ -268,11 +323,10 @@ git remote add origin git@ghblit:blitterated/tarmux.git
   end
 end
 
-
 def inspect_cli_option_immutability
   AllCLIOptions::each do |key, opt|
     desc = opt.description
-    desc.replace("#{key.to_s}: fart")
+    desc.replace("#{key.to_s}: I should cause an Error")
   end
 
   AllCLIOptions::each { |key, opt| puts opt.description }; puts
@@ -292,12 +346,26 @@ def debug_dump
   puts "repo_name: #{repo_name}"; puts
 
   proto_opts = AllCLIOptions::proto_option_defs
+  puts "AllCLIOptions::proto_option_defs"
   p proto_opts; puts
 
-  AllCLIOptions::each { |opt| p opt; puts }
+  cli_opts = AllCLIOptions::array
+  puts "AllCLIOptions::array"
+  p cli_opts; puts
 
-  arg_options = GitAuthorEditCLI.new.parse ARGV
-  p arg_options
+  sym2pkey = AllCLIOptions::symbols_and_param_keys
+  puts "AllCLIOptions::symbols_and_param_keys"
+  p sym2pkey; puts
+
+  puts "AllCLIOptions::each"
+  AllCLIOptions::each do |key, opt|
+    puts key.to_s
+    p opt
+    puts
+  end
+
+  config = GitAuthorEditCLI.new.parse ARGV
+  puts config; puts
 
   #inspect_cli_option_immutability
 
